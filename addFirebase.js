dad734c5220dc13724d76051f53d8eb46441696c
priyanshu-ya <priyanshuyadav267@gmail.com>
2022-11-09 23:00:11 +0530

    minor update

--- addFirebase.js
+++ addFirebase.js
@@ -1,16 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAISvJ7oquZN47lEIc-tIY8FNww4pUHMfg",
-    authDomain: "fluid-c4f9a.firebaseapp.com",
-    projectId: "fluid-c4f9a",
-    storageBucket: "fluid-c4f9a.appspot.com",
-    messagingSenderId: "700532619068",
-    appId: "1:700532619068:web:c615c7a4b42aba906d1039"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2CrDydNpDGHaUzm-A3Te-oIogQnyjF-I",
+  authDomain: "fluid-73ed6.firebaseapp.com",
+  projectId: "fluid-73ed6",
+  storageBucket: "fluid-73ed6.appspot.com",
+  messagingSenderId: "162069046592",
+  appId: "1:162069046592:web:2ba5679a08d1831885c6a5"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
