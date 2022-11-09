bf1266fbaee0b1fdfb37fff0d053af8b5fa74828
reenav01 <reenavh@sjcem.edu.in>
2022-11-09 18:30:55 +0530

    bhot kuc

---
+++ addFirebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAISvJ7oquZN47lEIc-tIY8FNww4pUHMfg",
+    authDomain: "fluid-c4f9a.firebaseapp.com",
+    projectId: "fluid-c4f9a",
+    storageBucket: "fluid-c4f9a.appspot.com",
+    messagingSenderId: "700532619068",
+    appId: "1:700532619068:web:c615c7a4b42aba906d1039"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
